8a680264d3f2a20e1b139f4ebef44083535721a6
AsimBhatia <59758774+AsimBhatia@users.noreply.github.com>
2020-03-15 06:47:21 -0400

    new homepage, new backgrounds

--- view/js/script.js
+++ view/js/script.js
@@ -1,3 +1,10 @@
+const name = document.querySelectorAll("#name path");
+
+// console.log(name);
+for(let i = 0; i < name.length; i++){
+	console.log(`Letter ${i} is ${name[i].getTotalLength()}`);
+}
+
 const nav = document.querySelector('.nav');
 nav.addEventListener('click',(event) => {
     nav.classList.toggle('open');
